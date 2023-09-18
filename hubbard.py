f9631fa2511b1bda1c7c5586a78064a0ebb0eb34
Chong Sun <sunchong137@gmail.com>
2023-09-18 12:06:47 -0500

    added filling

--- hubbard.py
+++ hubbard.py
@@ -1,23 +1,34 @@
 '''
-Hubbard model.
+Standard and disordered Hubbard model.
 '''
 import numpy as np
 import helpers
 from pyscf import gto, scf, ao2mo, fci
 from scipy import special
 
-def hubham_1d(norb, U, pbc=True):
-    h1e = np.zeros((norb, norb))
-    eri = np.zeros((norb,)*4)
+def hubham_1d(nsite, U, pbc=True):
+    '''
+    Return 1D Hubbard model Hamiltonians (h1e and h2e).
+    Unit: hopping amplitude t.
+    Args:
+        nsite : int, number of sites.
+        U: double, Hubbard U/t value.
+        pbc: boolean, if True, the system obeys periodic boundary condition.
+    Returns:
+        2D array: h1e
+        4D array: h2e
+    '''
+    h1e = np.zeros((nsite, nsite))
+    eri = np.zeros((nsite,)*4)
 
-    for i in range(norb-1):
+    for i in range(nsite-1):
         h1e[i, (i+1)] = -1.
         h1e[(i+1), i] = -1.
         eri[i,i,i,i] = U
     eri[-1,-1,-1,-1] = U
 
     if pbc:
-        # assert norb%4 == 2, "PBC requires Norb = 4n+2!"
+        # assert nsite%4 == 2, "PBC requires nsite = 4n+2!"
         h1e[0, -1] = -1.
         h1e[-1, 0] = -1.
     return h1e, eri
@@ -25,14 +36,14 @@ def hubham_1d(norb, U, pbc=True):
 def hamhub_2d():
     pass
 
-def hubham_noisy_1d(norb, U, pbc=True, max_w=1.0):
+def hubham_noisy_1d(nsite, U, pbc=True, max_w=1.0):
     '''
     Add diagonal noise to the original Hubbard model.
     The noise is a uniform distribution in [-max_w, max_w]
     '''
-    h1e, eri = hubham_1d(norb, U, pbc)
+    h1e, eri = hubham_1d(nsite, U, pbc)
     # generate uniform distribution
-    noise = (np.random.rand(norb)*2-1) * max_w
+    noise = (np.random.rand(nsite)*2-1) * max_w
     noise = np.diag(noise)
     return h1e+noise, eri
 
@@ -42,26 +53,31 @@ def hubham_spinless_1d():
     '''
     pass
 
-def hubbard_mf(norb, U, spin=0, nelec=None, pbc=True):
+def hubbard_mf(nsite, U, spin=0, nelec=None, pbc=True, filling=1.0):
     '''
     Mean-field of Hubbard model.
     Args:
-        norb: int, number of sites
-        U: positive float, Hubbard U
+        nsite: int, number of sites
+        U: positive float, Hubbard U/t
     Kwargs:
         spin: 0 (RHF) or 1 (UHF)
+        nelec: int, number of electrons.
         pbc: boolean, periodic boundary condition
+        filling: float, nelec/nsite. value 1 corresponds to half-filling.
     Returns:
         float: HF energy 
-        numpy array of size (nspin, norb, norb), the MO coefficients.
+        numpy array of size (nspin, nsite, nsite), the MO coefficients.
     '''
-    # Norb need to be 4n + 2, otherwise there is degeneracy between HOMO and LUMO
+    # nsite need to be 4n + 2, otherwise there is degeneracy between HOMO and LUMO
     mol = gto.M()
-    if nelec is None:
-        nelec = norb 
-    mol.nelectron = norb
 
-    h1e, eri = hubham_1d(norb, U, pbc=pbc)
+    if nelec is None:
+        nelec = int(nsite * filling + 1e-10)
+    if abs(nelec - nsite * filling) > 1e-2:
+        print("Warning: changing filling to {:0.2f} to keep integer number of electrons!".format(nelec/nsite))
+    
+    mol.nelectron = nsite
+    h1e, eri = hubham_1d(nsite, U, pbc=pbc)
     
     if spin == 0:
         mf = scf.RHF(mol)
@@ -71,8 +87,8 @@ def hubbard_mf(norb, U, spin=0, nelec=None, pbc=True):
         raise ValueError("Spin has to be 0 or 1!")
 
     mf.get_hcore = lambda *args: h1e 
-    mf.get_ovlp = lambda *args: np.eye(norb)
-    mf._eri = ao2mo.restore(8, eri, norb)
+    mf.get_ovlp = lambda *args: np.eye(nsite)
+    mf._eri = ao2mo.restore(8, eri, nsite)
     mol.incore_anyway = True
 
     if spin == 1:
@@ -81,25 +97,33 @@ def hubbard_mf(norb, U, spin=0, nelec=None, pbc=True):
         mf.kernel()
     return mf
 
-def hubbard_fci(norb, U, spin=0, nelec=None, pbc=True):
+def hubbard_fci(nsite, U, spin=0, nelec=None, pbc=True, filling=1.0):
     
-    h1e, eri = hubham_1d(norb, U, pbc)
-    cisolver = fci.direct_spin0
+    h1e, eri = hubham_1d(nsite, U, pbc)
+
     if nelec is None:
-        nelec = norb
+        nelec = int(nsite * filling + 1e-10)
+    if abs(nelec - nsite * filling) > 1e-2:
+        print("Warning: changing filling to {:0.2f} to keep integer number of electrons!".format(nelec/nsite))
+    
     # initial guess
     try:
         na, nb = nelec 
     except:
         na = nelec//2
         nb = nelec - na
-    
-    len_a = int(special.comb(norb, na) + 1e-10)
-    len_b = int(special.comb(norb, nb) + 1e-10)
-    ci0 = np.random.rand(len_a, len_b)
+
+    if na == nb:
+        cisolver = fci.direct_spin0
+    else:
+        cisolver = fci.direct_spin1
+        nelec = (na, nb)
+    # len_a = int(special.comb(nsite, na) + 1e-10)
+    # len_b = int(special.comb(nsite, nb) + 1e-10)
+    # ci0 = np.random.rand(len_a, len_b)
     # ci0 = np.ones((len_a, len_b)) / np.sqrt(len_a*len_b)
 
-    e, c = cisolver.kernel(h1e, eri, norb, nelec)
+    e, c = cisolver.kernel(h1e, eri, nsite, nelec)
     return e, c
 
 def hubbard_fci_from_mf(mf):
--- test/test_hubbard.py
+++ test/test_hubbard.py
@@ -44,7 +44,7 @@ def test_mf_rhf():
     assert np.allclose(mo, mo_coeff)
     
 def test_uhf():
-    norb = 6
+    norb = 18
     U = 8
     spin = 1
     mymf = hubbard.hubbard_mf(norb, U, spin=spin)
@@ -53,62 +53,45 @@ def test_uhf():
 
     # pyscf example
     mymol = gto.M() 
-    n = 6
-    mymol.nelectron = n
-    nelec = n
+    mymol.nelectron = norb
 
     mf = scf.UHF(mymol)
-    h1 = np.zeros((n,n))
-    for i in range(n-1):
+    h1 = np.zeros((norb, norb))
+    for i in range(norb - 1):
         h1[i,i+1] = h1[i+1,i] = -1.0
-    h1[n-1,0] = h1[0,n-1] = -1.0  # PBC
-    eri = np.zeros((n,n,n,n))
-    for i in range(n):
+    h1[norb-1,0] = h1[0,norb-1] = -1.0  # PBC
+    eri = np.zeros((norb,)*4)
+    for i in range(norb):
         eri[i,i,i,i] = U
 
     mf.get_hcore = lambda *args: h1
-    mf.get_ovlp = lambda *args: np.eye(n)
+    mf.get_ovlp = lambda *args: np.eye(norb)
     # ao2mo.restore(8, eri, n) to get 8-fold permutation symmetry of the integrals
     # ._eri only supports the two-electron integrals in 4-fold or 8-fold symmetry.
-    mf._eri = ao2mo.restore(8, eri, n)
+    mf._eri = ao2mo.restore(8, eri, norb)
 
     helpers.run_stab_mf(mf)
     e_hf = mf.energy_elec()[0]
     mo_coeff = mf.mo_coeff
 
     assert np.allclose(e, e_hf)
-   # print(mo[0] - mo[1])
-   # print(mo_coeff[0] - mo_coeff[1])
-    # dm = mf.make_rdm1()
-    # print(dm[0] - dm[1])
-    # mo_coeff = mymf.mo_coeff
-    # sdet = mo_coeff[:, :, :(n//2)]
-    # z = slater_site.det_z_det(n, sdet, x0=0)
-    # print(z)
-
-
-    ##assert np.allclose(e, e_hf)
-    #assert np.allclose(mo, mo_coeff)
 
 def test_fci():
     bethe = -0.573729
-    norb = 10
+    norb = 6
     U = 4
     pbc = True
     spin = 0
-    t1 = time.time()
+    # t1 = time.time()
     mymf = hubbard.hubbard_mf(norb, U, spin=spin, pbc=pbc)
     e_fci, ci = hubbard.hubbard_fci_from_mf(mymf)
-    # print(e_fci)
-    t2 = time.time()
+    # t2 = time.time()
     e_fci2, ci2 = hubbard.hubbard_fci(norb, U, pbc=pbc)
-    t3 = time.time()
+    # t3 = time.time()
     assert np.allclose(e_fci, e_fci2)
     # print(t2-t1, t3-t2)
-    # print(e_fci - e_fci2)
     # exit()
-    # compare to pyscf 
     e_per_site = e_fci / norb
     assert abs(e_per_site - bethe) < 1e-1
 
-test_fci()
+# test_fci()
