690bddfcba2bbcd041677866187b99cca65847a6
Chong Sun <sunchong137@gmail.com>
2024-02-24 09:40:05 -0600

    finite t

--- examples/05_spinless_ft_hf.py
+++ examples/05_spinless_ft_hf.py
@@ -3,6 +3,7 @@ from compol import slater_spinless
 from compol.hamiltonians import disorder_ham
 from compol.solvers import fci_uhf
 
+T = 0.5
 nsite = 30
 V = 2
 tprime = 0
@@ -10,7 +11,7 @@ W = 2
 pbc = True
 nelec = (nsite // 2, 0)
 obj = disorder_ham.spinless1d(nsite, V, W, tprime, pbc, 'box')
-mf = obj.run_scf()
+mf = obj.run_scf(T=T)
 mo_coeff = mf.mo_coeff[0]
 nocc = int(np.sum(mf.mo_occ[0]) + 1e-10)
 sdet = mo_coeff[:, :nocc]
